b9914859c4b42b96d14f0b443bf3f63b308f368e
Camilo <paula@pena-parra.ch>
2026-05-15 18:48:08 +0200

    implemented server-side update logic

--- app/Http/Controllers/Api/v1/ApiPollController.php
+++ app/Http/Controllers/Api/v1/ApiPollController.php
@@ -34,7 +34,10 @@ class ApiPollController extends Controller
 
         return $poll;
     }
-
+    
+    /**
+     * Create the specified poll.
+     */
     public function store(Request $request)
     {
         $validated = $request->validate([
@@ -68,6 +71,59 @@ class ApiPollController extends Controller
         $poll->options()->createMany($validated['options']);
 
         return $poll;
+    }
+
+    /**
+     * Update the specified poll.
+     */
+    public function update(Request $request, int $id)
+    {
+         $poll = Poll::find($id);
+
+        if (!$poll) {
+            return response()->json(['message' => 'Poll not found.'], 404);
+        }
+
+        if ($poll->user !== $request->user()) {
+            return response()->json(['message' => 'Unauthorized.'], 403);
+        }
+
+        if (!$poll->is_draft) {
+            return response()->json(['message' => 'Conflict : poll already published.'], 409);
+        }
+
+        $validated = $request->validate([
+            'question' => 'required|string|min:3|max:255',
+            'title' => 'nullable|string|max:255',
+            'options' => 'required|array|min:2',
+            'options.*.label' => 'required|string|min:1|max:255',
+            'allow_multiple_choices' => 'boolean',
+            'allow_vote_change' => 'boolean',
+            'results_public' => 'boolean',
+            'duration' => 'nullable|integer|min:60|max:8640000', // max: 100 jours
+            'ends_at' => 'nullable|date|after:now',
+            'is_draft' => 'boolean',
+        ]);
+
+        $publish = !$validated['is_draft'];
+
+        $poll->update([
+            'title' => $validated['title'],
+            'question' => $validated['question'],
+            'is_draft' => $validated['is_draft'],
+            'allow_multiple_choices' => $validated['allow_multiple_choices'],
+            'allow_vote_change' => $validated['allow_vote_change'],
+            'results_public' => $validated['results_public'],
+            'duration' => $validated['duration'],
+            'started_at' => $publish ? now() : null,
+            'ends_at' => $validated['ends_at'] ?? $publish && $validated['duration'] ? now()->addSeconds($validated['duration']) : null, // si une date est définie prend celle-là, sinon calcule en fonction de la durée si le sondage est publié
+        ]);
+
+        $poll->options()->delete();        
+        $poll->options()->createMany($validated['options']);
+
+        return $poll;
+
     }
 
     /**
